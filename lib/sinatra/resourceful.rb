e7e5fa45ba053376da41a33d4b08349c024f50c6
Kyle Drake <kyledrake@gmail.com>
2011-02-07 15:14:49 -0800

    Push instance vars into Config class so we can bind easily for code blocks. May push instances directly into this, or might write method to auto-create config from instance variables.

--- lib/sinatra/resourceful.rb
+++ lib/sinatra/resourceful.rb
@@ -1,3 +1,4 @@
+require 'ostruct'
 require 'linguistics'
 
 module Sinatra
@@ -7,6 +8,10 @@ module Sinatra
     end
     
     class Resource
+      
+      class Config < OpenStruct; end
+      attr_reader :config
+      
       ACTIONS = [:index, :new, :create, :show, :edit, :update, :delete]
       def initialize(app, model, actions, block)
         
@@ -17,90 +22,97 @@ module Sinatra
           actions = [*actions]
         end
         
-        model = model.name
-        singular = model.downcase.to_sym
-        plural = Linguistics::EN.plural(model.downcase).to_sym
+        @model = model.name
+        @singular = @model.downcase.to_sym        
+        @plural = Linguistics::EN.plural(@model.downcase).to_sym
         
         self.instance_eval(&block) if block
 
-        @redirect ||= "/#{plural}"
+        @redirect ||= "/#{@plural}"
         @create_redirect ||= @redirect
         @update_redirect ||= @redirect
         @before_actions ||= []
         @before_actions = ACTIONS if @before_actions.include?(:all)
         
-        redirect = @redirect
-        create_redirect = @create_redirect
-        update_redirect = @update_redirect
-        conditions = @conditions
-        before = @before
-        before_actions = @before_actions
+        # Containerize these with an OpenStruct, so we can bind them to a local variable for the block execution.
+        # The alternative is something crazy that writes local variables from instance variables.. not interested.
+        @config = Config.new :model => @model,
+                             :singular => @singular,
+                             :plural => @plural,
+                             :redirect => @redirect, 
+                             :create_redirect => @create_redirect, 
+                             :update_redirect => @update_redirect, 
+                             :conditions => @conditions,
+                             :before => @before,
+                             :before_actions => @before_actions
+        
+        _c = @config
         
         if actions.include? :index
-          app.get "/#{plural}/?" do
-            self.instance_eval &before if before_actions.include?(:index)
+          app.get "/#{_c.plural}/?" do
+            self.instance_eval &_c.before if _c.before_actions.include?(:index)
             self.instance_eval %{
-              @#{plural} = #{model}.all conditions
-              erb "#{plural}/index".to_sym
+              @#{_c.plural} = #{_c.model}.all _c.conditions
+              erb "#{_c.plural}/index".to_sym
             }
           end
         end
 
         if actions.include? :new
-          app.get "/#{plural}/new" do
-            self.instance_eval &before if before_actions.include?(:new)
+          app.get "/#{_c.plural}/new" do
+            self.instance_eval &_c.before if _c.before_actions.include?(:new)
             self.instance_eval %{
-              erb "#{plural}/new".to_sym
+              erb "#{_c.plural}/new".to_sym
             }
           end
         end
 
         if actions.include? :create
-          app.post "/#{plural}" do
-            self.instance_eval &before if before_actions.include?(:create)
+          app.post "/#{_c.plural}" do
+            self.instance_eval &_c.before if _c.before_actions.include?(:create)
             self.instance_eval %{
-              @#{plural} = #{model}.create params[:#{singular}]
-              redirect create_redirect
+              @#{_c.plural} = #{_c.model}.create params[:#{_c.singular}]
+              redirect _c.create_redirect
             }
           end
         end
 
         if actions.include? :update
-          app.put "/#{plural}/:id" do
-            self.instance_eval &before if before_actions.include?(:update)
+          app.put "/#{_c.plural}/:id" do
+            self.instance_eval &_c.before if _c.before_actions.include?(:update)
             self.instance_eval %{
-              #{singular} = #{model}.get(params[:id]).update params[:#{singular}]
-              redirect update_redirect
+              #{_c.singular} = #{_c.model}.get(params[:id]).update params[:#{_c.singular}]
+              redirect _c.update_redirect
             }
           end
         end
 
         if actions.include? :edit
-          app.get "/#{plural}/:id/edit" do
-            self.instance_eval &before if before_actions.include?(:edit)
+          app.get "/#{_c.plural}/:id/edit" do
+            self.instance_eval &_c.before if _c.before_actions.include?(:edit)
             self.instance_eval %{
-              @#{singular} = #{model}.get params[:id]
-              erb :"#{plural}/edit"
+              @#{_c.singular} = #{_c.model}.get params[:id]
+              erb :"#{_c.plural}/edit"
             }
           end
         end
 
         if actions.include? :show
-          app.get "/#{plural}/:id" do
-            self.instance_eval &before if before_actions.include?(:show)
+          app.get "/#{_c.plural}/:id" do
+            self.instance_eval &_c.before if _c.before_actions.include?(:show)
             self.instance_eval %{
-              @#{singular} = #{model}.get params[:id]
-              erb :"#{plural}/show"
+              @#{_c.singular} = #{_c.model}.get params[:id]
+              erb :"#{_c.plural}/show"
             }
           end
         end
 
         if actions.include? :delete
-          app.delete "/#{plural}/:id" do
-            self.instance_eval &before if before_actions.include?(:delete)
+          app.delete "/#{_c.plural}/:id" do
+            self.instance_eval &_c.before if _c.before_actions.include?(:delete)
             self.instance_eval %{
-              #{model}.get(params[:id]).destroy
-              redirect "/#{plural}"
+              #{_c.model}.get(params[:id]).destroy
+              redirect "/#{_c.plural}"
             }
           end
         end
